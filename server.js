70484d8189290603ca292293c5c9e752c5bb5322
giorgiospagnolo21-wq <giorgiospagnolo21@gmail.com>
2025-12-17 11:48:10 +0100

    Add vote route to server

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use('/api/login', require('./routes/login'));
 app.use('/api/upload', require('./routes/upload'));
 app.use('/api/delete', require('./routes/delete'));
 app.use('/api/posters', require('./routes/posters'));
+app.use('/api/vote', require('./routes/vote'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server attivo su http://localhost:${PORT}`));
